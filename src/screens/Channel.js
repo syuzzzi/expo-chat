addcdeaff7b1d84ceca55eb502b951cf7c08b553
csj1430 <csj1430@naver.com>
2025-02-15 18:23:39 +0900

    done

--- src/screens/Channel.js
+++ src/screens/Channel.js
@@ -1,21 +1,99 @@
-import React from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import styled from "styled-components/native";
+import { createMessage, getCurrentUser, app } from "../firebase";
+import { GiftedChat, Send } from "react-native-gifted-chat";
+import { Alert } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  query,
+  doc,
+  orderBy,
+} from "firebase/firestore";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.background};
 `;
-const StyledText = styled.Text`
-  font-size: 30px;
-`;
+const SendIcon = styled(MaterialIcons).attrs(({ theme, text }) => ({
+  name: "send",
+  size: 24,
+  color: text ? theme.sendBtnActive : theme.sendBtnInactive,
+}))``;
 
-const Channel = ({ route }) => {
+const SendButton = (props) => {
   return (
-    <Container>
-      <StyledText>Channel</StyledText>
-      <StyledText>{route.params.id}</StyledText>
-      <StyledText>{route.params.title}</StyledText>
-    </Container>
+    <Send
+      {...props}
+      containerStyle={{
+        width: 44,
+        height: 44,
+        alignItems: "center",
+        justifyContent: "center",
+        marginHorizontal: 4,
+      }}
+      disabled={!props.text}
+    >
+      <SendIcon text={props.text} />
+    </Send>
+  );
+};
+
+const Channel = ({ navigation, route }) => {
+  const [messages, setMessages] = useState([]);
+  const { uid, name, photo } = getCurrentUser();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: route.params.title || "Channel",
+    });
+  }, []);
+
+  const db = getFirestore(app);
+  useEffect(() => {
+    const docRef = doc(db, "channels", route.params.id);
+    const collectionQuery = query(
+      collection(db, `${docRef.path}/messages`),
+      orderBy("createdAt", "desc")
+    );
+    const unsubscribe = onSnapshot(collectionQuery, (snapshot) => {
+      const list = [];
+      snapshot.forEach((doc) => {
+        list.push(doc.data());
+      });
+      setMessages(list);
+    });
+    return () => unsubscribe(); // 꼭 있어야 함!
+  }, []);
+
+  const _handleMessageSend = async (messageList) => {
+    const message = messageList[0];
+    try {
+      await createMessage({ channelId: route.params.id, message });
+    } catch (e) {
+      Alert.alert("Message Error", e.message);
+    }
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Container>
+        <GiftedChat
+          placeholder="Enter a message ..."
+          messages={messages}
+          user={{ _id: uid, name, avatar: photo }}
+          onSend={_handleMessageSend}
+          renderSend={(props) => <SendButton {...props} />}
+          scrollToBottom={true}
+          renderUsernameOnMessage={true}
+          alwaysShowSend={true}
+          multiline={false}
+        />
+      </Container>
+    </SafeAreaView>
   );
 };
 
